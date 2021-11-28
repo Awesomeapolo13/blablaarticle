18334c4dab0dc874df2e2d91da162873fa548a3f
Awesomeapolo13 <aelog16@yandex.ru>
2021-11-28 15:58:07 +0300

    Authentication system complete

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * Отображает страницу авторизации пользователя и выводит оишибки авторизации
+     *
+     * @Route("/login", name="app_login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // достает текст последней ошибки авторизации
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // достает последний логин
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        dump($lastUsername);
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * Отвечает за выход пользователя
+     *
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+}
