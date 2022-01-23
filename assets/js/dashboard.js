bcd48086d790ad2e0e8c81275f492da5e3106c3f
Awesomeapolo13 <aelog16@yandex.ru>
2022-01-23 18:11:32 +0300

    Functionality for adding and removing promoted word fields finished.

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -8,10 +8,21 @@ const jQuery = require('jquery');
         $("#wrapper").toggleClass("toggled");
     });
 
-    // Обработчик кнопки добавления продвигаемого слова
-    $(".add-promoted-word").on('click', function (e) {
+    // Кнопка добавления полей продвигаемого слова
+    const addPromotedWordButton = $('.add-promoted-word');
+    // Кнопка удаления полей продвигаемого слова
+    const removePromotedWordButton = $(".remove-promoted-word");
+    // Блок с полями продвигаемого слова
+    const list = $(addPromotedWordButton.attr('data-list-selector'));
+
+    hideOrShowRemoveFieldButton(list, removePromotedWordButton);
+
+    /**
+     * Обработчик кнопки добавления полей для продвигаемого слова
+     */
+    addPromotedWordButton.on('click', function (e) {
         e.preventDefault();
-        let list = $($(this).attr('data-list-selector'));
+        const list = $($(this).attr('data-list-selector'));
 
         for (let key = 0; key <= 1; key++) {
             const elem = list[key];
@@ -25,8 +36,35 @@ const jQuery = require('jquery');
 
             let newElem = $(elem.getAttribute('data-widget-tags')).html(newWidget);
             newElem.appendTo(elem);
+
+            hideOrShowRemoveFieldButton(list, removePromotedWordButton);
         }
 
+    });
+
+    /**
+     * Обработчик кнопки удаления полей для продвигаемого слова
+     */
+    removePromotedWordButton.on('click', function (e) {
+        e.preventDefault();
+        const list = $($(this).attr('data-list-selector'));
+
+        for (let key = 0; key <= 1; key++) {
+            const elem = list[key];
+            const lastElement = elem.childNodes[(elem.childNodes.length) - 1];
+            lastElement.remove();
+            hideOrShowRemoveFieldButton(list, removePromotedWordButton)
+        }
     })
+
+    /**
+     * Скрывает или отображает кнопку удаления полей продвигаемого слова
+     *
+     * @param fieldsList - блок с полями продвигаемого слова, выбранный JQuery селектором
+     * @param button - кнопка выбранная с помощью JQuery селектора
+     */
+    function hideOrShowRemoveFieldButton(fieldsList, button) {
+        fieldsList.children().length <= 2 ? button.hide() : button.show();
+    }
 
 })(jQuery);
