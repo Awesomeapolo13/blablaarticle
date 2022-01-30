dd63870bf96715832700a8804230eef137ba40f7
Awesomeapolo13 <aelog16@yandex.ru>
2022-01-31 00:09:04 +0300

    Subscription page and controller added.

---
+++ src/Controller/Admin/SubscriptionController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SubscriptionController extends AbstractController
+{
+    /**
+     * Отображает страницу с информацией о подписке пользователя
+     *
+     * @Route("/admin/subscription", name="app_admin_subscription" )
+     * @param Request $request
+     * @return Response
+     */
+    public function index(
+        Request $request
+    ): Response
+    {
+        /**
+         * ToDo:
+         *      1) Создать сущность subscription, подумать что в нее должно входить.
+         *      2) Создать миграцию для таблицы подписки
+         *      3) Составить фикстуры для подписок
+         *      4) Выполнить вывод всех подписок из базы на страницу и ограничить доступ к админке для неавторизованных
+         *          пользователей
+         *      5) Реализовать механизм изменения подписки в соостветствии с ТЗ
+         */
+
+        return $this->render('admin/subscription/index.html.twig', []);
+    }
+}
