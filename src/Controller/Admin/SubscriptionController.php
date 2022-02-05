3894a6b272a546b3212f23d33cb433b261dc5786
Alexey Logachev <alexey.logachev@norbit.ru>
2022-02-05 15:38:12 +0300

    The mechanism for changing subscription added.

--- src/Controller/Admin/SubscriptionController.php
+++ src/Controller/Admin/SubscriptionController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Repository\SubscriptionRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,24 +17,62 @@ class SubscriptionController extends AbstractController
      *
      * @Route("/admin/subscription", name="app_admin_subscription" )
      * @IsGranted("IS_AUTHENTICATED_FULLY")
+     * @param Request $request
      * @param SubscriptionRepository $subscriptionRepository
      * @return Response
      */
-    public function index(SubscriptionRepository $subscriptionRepository): Response
+    public function index(
+        Request                $request,
+        SubscriptionRepository $subscriptionRepository
+    ): Response
     {
-        /**
-         * ToDo:
-         *      1) Выполнить вывод всех подписок из базы на страницу и ограничить доступ к админке для неавторизованных
-         *          пользователей
-         *      2) Реализовать механизм изменения подписки в соостветствии с ТЗ
-         */
         $user = $this->getUser();
 
-        $subscriptions = $subscriptionRepository->findAll();
+        $subscriptions = $subscriptionRepository->findSubscriptionsOrderedByPrice();
 
         return $this->render('admin/subscription/index.html.twig', [
             'subscriptions' => $subscriptions,
-            'userSubscriptionId' => ($user->getSubscription())->getId(),
+            'userSubscription' => $user->getSubscription(),
+            'expireAt' => $user->getExpireAt(),
+            'change' => $request->query->get('change'),
+        ]);
+    }
+
+    /**
+     * Меняет подписку у пользователя
+     *
+     * @Route("/admin/subscription/{id}", name="app_admin_subscription_change")
+     * @IsGranted("IS_AUTHENTICATED_FULLY")
+     * @param int $id - идентификатор подписки
+     * @param EntityManagerInterface $em
+     * @param SubscriptionRepository $subscriptionRepository
+     * @return Response
+     */
+    public function changeSubscription(
+        int                    $id,
+        EntityManagerInterface $em,
+        SubscriptionRepository $subscriptionRepository
+    ): Response
+    {
+        $success = false;
+        $user = $this->getUser();
+        $newSubscription = $subscriptionRepository->findOneBy(['id' => $id]);
+
+        // нельзя понизить текущий уровень подписки
+        if (($user->getSubscription())->getPrice() < $newSubscription->getPrice()) {
+            $user
+                ->setSubscription($newSubscription)
+                ->setExpireAt(new \DateTime('+1 week'))
+            ;
+
+            $em->persist($user);
+            $em->flush();
+
+            $success = true;
+        }
+
+        return $this->redirectToRoute('app_admin_subscription', [
+            'change' => $success,
         ]);
     }
 }
