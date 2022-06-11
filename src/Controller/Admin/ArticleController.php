788dcdf14664c73815ba98c71a95abb30c7a131d
ALogachev <aelog16@yandex.ru>
2022-06-12 00:35:37 +0300

    Free Article strategy finished

--- src/Controller/Admin/ArticleController.php
+++ src/Controller/Admin/ArticleController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller\Admin;
 
+use App\ArticleGeneration\ArticleGenerator;
+use App\ArticleGeneration\Strategy\FreeArticleGenerationStrategy;
+use App\Entity\Article;
 use App\Form\ArticleGenerationFormType;
 use App\Form\Model\ArticleFormModel;
 use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Knp\Component\Pager\PaginatorInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -53,28 +58,57 @@ class ArticleController extends AbstractController
      *
      * @Route("/admin/article/create", name="app_admin_article_create")
      * @param Request $request
+     * @param ArticleGenerator $articleGenerator
+     * @param FreeArticleGenerationStrategy $freeStrategy
      * @return Response
+     * @throws Exception
      */
     public function create(
-        Request $request
+        Request                       $request,
+        ArticleGenerator              $articleGenerator,
+        FreeArticleGenerationStrategy $freeStrategy,
+        EntityManagerInterface        $em,
+        ArticleRepository             $articleRepository
     ): Response
     {
+        // TODo: Выполнить лимитирование генерации статьи на этапе реализации для API
         $form = $this->createForm(ArticleGenerationFormType::class);
         $form->handleRequest($request);
-
-        $success = false;
+        $article = $request->get('articleId')
+            ?
+            $articleRepository->findOneBy([
+                'id' => $request->get('articleId'),
+            ])
+            :
+            false;
 
         if ($form->isSubmitted() && $form->isValid()) {
             /** @var ArticleFormModel $articleModel */
             $articleModel = $form->getData();
-            dump($articleModel);
-            $success = true;
+            $article = Article::create($articleModel);
+
+            $em->persist(
+                $article
+                    ->setBody(
+                        $articleGenerator
+                            ->setArticleDTO($article)
+                            ->setGenerationStrategy($freeStrategy)
+                            ->generateArticle()
+                    )
+            );
+            $em->flush();
+
+            // Сообщение об успешном создании модуля
+            $this->addFlash('success', 'Статья успешно сгенерирована');
+            return $this->redirectToRoute('app_admin_article_create', [
+                'articleId' => $article->getId(),
+                ]);
         }
 
         return $this->render('admin/article/create.html.twig', [
             'articleForm' => $form->createView(),
             'errors' => $form->getErrors(true), // ошибки в форме
-            'success' => $success
+            'article' => $article,
         ]);
     }
 
